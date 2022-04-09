66efe3ffdf842e8105c84c7f9627dbec2e833faa
kurogoma939 <kan.sss.939@gmail.com>
2022-04-10 02:00:52 +0900

    🆕 go_routerの設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:chat_app_basic/ui/pages/auth/create_user.dart';
+import 'package:chat_app_basic/ui/pages/auth/login_page.dart';
 import 'package:chat_app_basic/ui/pages/chat_solo_page.dart';
 import 'package:chat_app_basic/ui/pages/friends_list_page.dart';
 import 'package:chat_app_basic/ui/pages/settings_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 Future<void> main() async {
   // Firebase初期化
@@ -19,17 +22,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chatアプリ',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  Widget build(BuildContext context) => MaterialApp.router(
+    routeInformationParser: _router.routeInformationParser,
+    routerDelegate: _router.routerDelegate,
+    title: 'レビューアプリ',
+  );
+
+  /// ルーティング設定
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/', // ベース：認証状態を識別してホーム画面orログインへ遷移させる
+        builder: (BuildContext context, GoRouterState state) => const ChatScreen(),
       ),
-      home: const ChatScreen(),
-    );
-  }
+      GoRoute(
+        path: '/login', // ログイン画面
+        builder: (BuildContext context, GoRouterState state) => const MailLoginPage(),
+      ),
+      GoRoute(
+        path: '/createUser', // 新規ユーザー登録画面
+        builder: (BuildContext context, GoRouterState state) => const RegistUserWithMail(),
+      ),
+      GoRoute(
+        path: '/setting', // ユーザープロフィール画面
+        builder: (BuildContext context, GoRouterState state) => const SettingsPage(),
+      ),
+      // GoRoute(
+      //   path: '/reviewDetail/:id', // レビュー詳細ページ
+      //   builder: (context, state) {
+      //     // パスパラメータの値を取得するには state.params を使用
+      //     final int id = int.parse(state.params['id']!);
+      //     return ReviewDetail(id: id);
+      //   },
+      // ),
+    ],
+    initialLocation: '/',
+  );
 }
 
 class ChatScreen extends StatefulWidget {
