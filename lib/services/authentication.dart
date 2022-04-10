f74a79e962eb0813aed8613c357c235c72dca652
kurogoma939 <kan.sss.939@gmail.com>
2022-04-10 11:25:40 +0900

    ♻️ 認証ロジックの切り離し

---
+++ lib/services/authentication.dart
@@ -0,0 +1,109 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+// ログインユーザーの情報を取得
+Future getLoggedInUser() async {
+  return FirebaseAuth.instance.currentUser;
+}
+
+// ログイン処理（メール）
+Future login(String email, String password) async {
+  try {
+    // メール/パスワードでログイン
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final UserCredential result = await auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    // ログインに成功した場合
+    final User user = result.user!;
+    return 'success';
+  } on FirebaseAuthException catch (e) {
+    // ログインに失敗した場合
+    String message = '';
+    // エラーコード別処理
+    switch (e.code) {
+      case 'invalid-email':
+        message = 'メールアドレスが不正です。';
+        break;
+      case 'wrong-password':
+        message = 'パスワードが違います。';
+        break;
+      case 'user-disabled':
+        message = '指定されたユーザーは無効です。';
+        break;
+      case 'user-not-found':
+        message = '指定されたユーザーは存在しません。';
+        break;
+      case 'operation-not-allowed':
+        message = '指定されたユーザーはこの操作を許可していません。';
+        break;
+      case 'too-many-requests':
+        message = '複数回リクエストが発生しました。';
+        break;
+      case 'email-already-exists':
+        message = '指定されたメールアドレスは既に使用されています。';
+        break;
+      case 'internal-error':
+        message = '内部処理エラーが発生しました。';
+        break;
+      default:
+        message = '予期せぬエラーが発生しました。';
+    }
+
+    return message;
+  }
+}
+
+// ユーザー作成（メール）
+Future registWithEmail(String email, String password) async {
+  try {
+    // メール/パスワードでユーザー登録
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final UserCredential result = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    /// そのままログインしてHomeへ遷移
+    final UserCredential login = await auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    // ログインに成功した場合
+    final User user = login.user!;
+    return 'success';
+  } on FirebaseAuthException catch (e) {
+    // ログインに失敗した場合
+    String message = '';
+    // エラーコード別処理
+    switch (e.code) {
+      case 'invalid-email':
+        message = 'メールアドレスが不正です。';
+        break;
+      case 'wrong-password':
+        message = 'パスワードが違います。';
+        break;
+      case 'user-disabled':
+        message = '指定されたユーザーは無効です。';
+        break;
+      case 'user-not-found':
+        message = '指定されたユーザーは存在しません。';
+        break;
+      case 'operation-not-allowed':
+        message = '指定されたユーザーはこの操作を許可していません。';
+        break;
+      case 'too-many-requests':
+        message = '複数回リクエストが発生しました。';
+        break;
+      case 'email-already-exists':
+        message = '指定されたメールアドレスは既に使用されています。';
+        break;
+      case 'internal-error':
+        message = '内部処理エラーが発生しました。';
+        break;
+      default:
+        message = '予期せぬエラーが発生しました。';
+    }
+    return message;
+  }
+}
