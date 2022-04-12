6ccaaf7c4be58ea28afcb6ce981539f2de1d45b5
kurogoma939 <kan.sss.939@gmail.com>
2022-04-13 01:21:40 +0900

    #4 ともだち一覧をFirebase・Storageから取得

---
+++ lib/ui/pages/chat/friends_list_page.dart
@@ -0,0 +1,103 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+/// TODO: ログインユーザーが所持するともだち一覧を表示
+
+class MessageWidget extends StatefulWidget {
+  const MessageWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MessageWidget> createState() => _MessageWidgetState();
+}
+
+class _MessageWidgetState extends State<MessageWidget> {
+  List<DocumentSnapshot> friendList = [];
+  List<DocumentSnapshot> userList = [];
+  List<dynamic> uidList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      // ログインユーザー取得
+      final user = FirebaseAuth.instance.currentUser;
+      // friendsコレクションから、友達のUID一覧を取得
+      var snapshot = await FirebaseFirestore.instance.collection('friends').where('hostUid', isEqualTo: user!.uid).get();
+      setState(() {
+        friendList = snapshot.docs;
+      });
+      for (var element in friendList) {
+        uidList.add(element['friendsUid']);
+      }
+      var userSnapshot = await FirebaseFirestore.instance.collection('users').where('uid', whereIn: uidList[0]).get();
+      setState(() {
+        userList = userSnapshot.docs;
+      });
+    });
+  }
+
+  test() {
+    print(uidList[0]);
+    print(friendList);
+    print(friendList[0]);
+    print(userList);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 2,
+        title: const Text(
+          'ともだち一覧',
+          style: TextStyle(color: Colors.black87),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search),
+            color: Colors.black87,
+            onPressed: () {
+              test();
+            },
+          )
+        ],
+      ),
+      body: SizedBox(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // Mapにしてリスト表示をする
+          children: userList.map((document) {
+            return GestureDetector(
+              child: ListTile(
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 8,
+                ),
+                leading: SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: ClipOval(
+                    child: Image.network(document['iconPath']),
+                  ),
+                ),
+                trailing: const Text('3分前'),
+                title: Text(document['userName']),
+                subtitle: Text(document['comment'].toString()),
+              ),
+              onTap: () {
+                // チャットページへ遷移 /chat/ログインユーザーのUID/選択した友達のUID
+              },
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
