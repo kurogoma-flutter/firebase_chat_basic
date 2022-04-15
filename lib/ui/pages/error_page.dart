3a6d0ff57514caaa8be323bdaffb5d6109f861a3
kurogoma939 <kan.sss.939@gmail.com>
2022-04-15 17:13:22 +0900

    エラーページ修正

--- lib/ui/pages/error_page.dart
+++ lib/ui/pages/error_page.dart
@@ -8,22 +8,30 @@ class ErrorPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Error !!',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500, color: Colors.red),
+      body: SizedBox(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: const Text(
+                'Error !!',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500, color: Colors.red),
+              ),
             ),
-          ),
-          TextButton(
-            onPressed: () {
-              context.go('/');
-            },
-            child: const Text('ホームへ戻る'),
-          ),
-        ],
+            TextButton(
+              onPressed: () {
+                context.go('/');
+              },
+              child: const Text(
+                'ホームへ戻る',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
