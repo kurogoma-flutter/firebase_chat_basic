c724e4c8ab92179337f4fbc1e041dc071d40c799
Kurogoma939 <67848399+Kurogoma939@users.noreply.github.com>
2022-04-26 13:47:27 +0900

    Firebaseの構成変更に伴いクラスの変更

--- lib/entity/user.dart
+++ lib/entity/user.dart
@@ -1,6 +1,8 @@
 class User {
   final String uid;
   final String name;
+  final String iconPath;
+  final String comment;
   final bool isBanned;
 
 //<editor-fold desc="Data Methods">
@@ -8,30 +10,36 @@ class User {
   const User({
     required this.uid,
     required this.name,
+    required this.iconPath,
+    required this.comment,
     required this.isBanned,
   });
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      (other is User && runtimeType == other.runtimeType && uid == other.uid && name == other.name && isBanned == other.isBanned);
+      (other is User && runtimeType == other.runtimeType && uid == other.uid && name == other.name && isBanned == other.isBanned && iconPath == other.iconPath && comment == other.comment);
 
   @override
-  int get hashCode => uid.hashCode ^ name.hashCode ^ isBanned.hashCode;
+  int get hashCode => uid.hashCode ^ name.hashCode ^ isBanned.hashCode ^ isBanned.iconPath ^ isBanned.comment;
 
   @override
   String toString() {
-    return 'User{' + ' uid: $uid,' + ' name: $name,' + ' isBanned: $isBanned,' + '}';
+    return 'User{' + ' uid: $uid,' + ' name: $name,' + ' isBanned: $isBanned,' +  ' iconPath: $iconPath,' +  ' comment: $comment,' + '}';
   }
 
   User copyWith({
     String? uid,
     String? name,
+    String? iconPath,
+    String? comment,
     bool? isBanned,
   }) {
     return User(
       uid: uid ?? this.uid,
       name: name ?? this.name,
+      iconPath: iconPath ?? this.iconPath,
+      comment: comment ?? this.comment,
       isBanned: isBanned ?? this.isBanned,
     );
   }
@@ -40,6 +48,8 @@ class User {
     return {
       'uid': this.uid,
       'name': this.name,
+      'iconPath': this.iconPath,
+      'comment': this.comment,
       'isBanned': this.isBanned,
     };
   }
@@ -48,6 +58,8 @@ class User {
     return User(
       uid: map['uid'] as String,
       name: map['name'] as String,
+      iconPath: map['iconPath'] as String,
+      comment: map['nacommentme'] as String,
       isBanned: map['isBanned'] as bool,
     );
   }
