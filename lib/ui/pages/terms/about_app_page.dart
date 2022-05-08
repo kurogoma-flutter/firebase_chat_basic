184717b45ac92e6741d4eb1177953f90d8376c93
kurogoma939 <kan.sss.939@gmail.com>
2022-05-09 00:24:06 +0900

    規約：このアプリについて画面作成

--- lib/ui/pages/terms/about_app_page.dart
+++ lib/ui/pages/terms/about_app_page.dart
@@ -26,6 +26,30 @@ class AboutAppPage extends StatelessWidget {
           backgroundColor: Theme.of(context).canvasColor,
           elevation: 2,
         ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Text(
+                'アプリタイトル',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'ver 1.0.0',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
